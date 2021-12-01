cc03cb006d5df442b63fa75b2379d7b40b400b08
User1910 <19.user.10@gmail.com>
2021-12-01 14:33:19 +0200

    add file

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+
 print('Hello,world!')
+
+word = input("Напишите слово: ")
+for char in range(len(word) - 1, -1, -1):
+  print(word[char], end="")
+print("\n")
+
+A = int(input())
+B = int(input())
+if A < B:
+  for i in range(A, B + 1):
+    print(i)
+else:
+  for i in reversed(range(B, A + 1)):
+    print(i)
+
+a = int(input())
+b = int(input())
+for i in range(a, b + 1):
+    print(i)
+
--- stepic.py
+++ stepic.py
@@ -1,1 +1,17 @@
 
+a = int(input())
+b = int(input())
+c = input()
+if c == str('+'):
+    print(a + b)
+elif c == str('-'):
+    print(a - b)
+elif c == str('*'):
+    print(a * b)
+elif c == str('/'):
+    if a != 0 and b != 0:
+        print(a / b)
+    else:
+        print('На ноль делить нельзя!')
+else:
+    print('Неверная операция')
